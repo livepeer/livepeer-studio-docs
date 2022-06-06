1a61c780007c0219026cd7ecca24a3d7066eb72f
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-06-06 14:15:22 -0500

    initial docs setup

---
+++ sidebars.js
@@ -0,0 +1,15 @@
+const guidesToc = require("./docs/hodl-guides/toc")
+const referencesToc = require("./docs/references/toc")
+const buidlToc = require("./docs/buidl-quick-start/toc");
+
+
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+ buidl: buidlToc,
+ guides: guidesToc,
+ referencesToc:  referencesToc
+};
+
+module.exports = sidebars;
