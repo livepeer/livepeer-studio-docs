df172564925199dcf8cac4718ad7075a9daa68b8
adamsoffer <adam@livepeer.org>
2022-09-02 17:20:00 -0400

    Update edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/livepeer/studio/tree/main/packages/main/",
+            "https://github.com/livepeer/livepeer-studio-docs/tree/main/packages/main/",
         },
         blog: false,
         theme: {
