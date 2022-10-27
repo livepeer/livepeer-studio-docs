fa91ea1eae21f14c98213d215a66dfc634ae460c
Suhail Kakar <suhail.zahed@gmail.com>
2022-10-27 17:55:55 +0530

    docs: improved the video playback guide

---
+++ src/components/Player/HTML/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function HTMLPlayer() {
+  return (
+    <div style={{ width: "100%" }}>
+      <video
+        style={{ width: "100%" }}
+        controls
+        src="https://livepeercdn.com/asset/6d7el73r1y12chxr/video"
+      />
+    </div>
+  );
+}
